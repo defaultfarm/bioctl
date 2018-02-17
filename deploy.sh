7aade843bbe0aef5f743c5facaea20e0534276d3
Timothy Klim <klimtimothy@gmail.com>
2018-02-17 09:40:21 +0300

    chore: use rsync for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# rustup target add armv7-unknown-linux-gnueabihf
+rustup target add armv7-unknown-linux-gnueabihf
 
 cargo build --target=armv7-unknown-linux-gnueabihf --release
-scp target/armv7-unknown-linux-gnueabihf/release/bioctl resin:/tmp/
+
+rsync -avz target/armv7-unknown-linux-gnueabihf/release/bioctl resin:/tmp/
 ssh resin /tmp/bioctl
